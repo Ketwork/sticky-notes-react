005a2d97045bf57fdb2df90e181bcea71370c0bc
Ket Patel <ketz555@hotmail.com>
2023-01-21 15:00:14 +0000

    added reducer + added new note function,  added styling for note and note text

--- src/App.js
+++ src/App.js
@@ -1,8 +1,45 @@
-import React, {useState} from 'react';
+import React, {useState, useReducer} from 'react';
+import { v4 as uuid } from 'uuid'; // function to provide unique identifier
+
+const initialNotesState = {
+  lastNoteCreated: null,
+  totalNotes: 0,
+  notes: [],
+};
+
+const notesReducer = (prevState, action) => {
+  switch (action.type) {
+    case 'ADD_NOTE': {
+      const newState = { 
+        notes: [...prevState.notes, action.payload],
+        totalNotes: prevState.notes.length + 1,
+        lastNoteCreated: new Date().toTimeString().slice(0, 8),
+      };
+      console.log('After ADD_NOTE: ', newState);
+            return newState;
+    }
+  }
+}
 
 export function App() {
     const [noteInput, setNoteInput] = useState('');
-    const addNote = () => {};
+    const [nostesState, dispatch] = useReducer(notesReducer, initialNotesState)
+    
+    const addNote = event => {
+      event.preventDefault();
+
+      if(!noteInput) {
+        return
+      }
+
+      const newNote = {
+        id: uuid(),
+        text: noteInput, 
+        rotate: Math.floor(Math.random() * 20)
+      }
+
+      dispatch({ type: 'ADD_NOTE', payload: newNote }); //sends to redux store (gets passed to the action argument in notesReducer)
+    };
 
     return (
         <div className="app">
@@ -17,7 +54,15 @@ export function App() {
               <button>Add</button>
             </form>
 
-            { noteInput }
+            {nostesState
+              .notes
+              .map(note => (
+                <div className='note'>
+                  <pre className='text'>{note.text}</pre>
+                </div>
+              ))
+            }
+
         </div>
     );
 }
